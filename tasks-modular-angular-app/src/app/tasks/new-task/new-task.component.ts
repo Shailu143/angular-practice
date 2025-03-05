6f21076004e3339ceec7a95e074d9f76d48b162c
Shailesh Tukaram Pagade <shailesh@macmini.mynetworksettings.com>
2025-03-04 22:42:45 -0500

    added all projects to git

---
+++ tasks-modular-angular-app/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, inject, Input, Output, signal } from '@angular/core';
+
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  @Input({required: true}) userId!: string;
+  //@Output() cancel = new EventEmitter<void>();
+  @Output() close = new EventEmitter<void>();
+  //@Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle ='';
+  enteredSummary='';
+  enteredDate='';
+  private taskService = inject(TasksService);
+
+  onCancel(){
+    //this.cancel.emit();
+    this.close.emit();
+  }
+
+  onSubmit(){
+    // this.add.emit({
+    //   title: this.enteredTitle,
+    //   summary: this.enteredSummary,
+    //   date: this.enteredDate
+    // });
+    this.taskService.addTask({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate
+      },
+      this.userId
+    )
+    this.close.emit();
+  }
+}
---
+++ tasks-modular-angular-app/src/app/tasks/task/task.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { Task } from './task.model';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input({required: true}) task!: Task;
+  private taskService = inject(TasksService);
+
+  onCompleteTask(){
+    this.taskService.removeTask(this.task.id);
+  }
+
+
+}
